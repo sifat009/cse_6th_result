b8896ab124759422895171fdadfba39dc29a0d81
sifat009 <bhacker150@gmail.com>
2017-10-19 13:28:54 +0600

    added failure text and some color

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ let resetButton = document.querySelector('#reset');
 let getGrades = function() {
 	let selectors = document.querySelectorAll('.custom-select');
 	selectors = Array.from(selectors);
-	let grades = selectors
-								.map(selector => selector.value)
-								.map(Number);
+	let grades = selectors.map(selector => selector.value != 0 ? selector.value : 'F').map(Number);
 
 	let result = (calculateGpa(grades)/sumOfCredits).toFixed(2);
-	result = isNaN(result) ? "<h3>Please select all the fields</h3>" : '<h3>Your GPA = '+result+' </h3>';
+	result = isNaN(result) ? 
+				"<h3>Please select all the fields  or</h3> <span class='redText'> May be you are Failed in any Subject</span>"
+				: '<h3>Your GPA = '+result+' </h3>';
 	resultDiv.innerHTML = result;
 }
 
